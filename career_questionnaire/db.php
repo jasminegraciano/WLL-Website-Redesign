6d825cd78f690ef7cc76ed3ae0bb534706c69693
Just-A-Dog <147770645+Just-A-Dog@users.noreply.github.com>
2025-04-21 22:28:15 -0400

    Fixing decoding JSON bug

--- career_questionnaire/db.php
+++ career_questionnaire/db.php
@@ -10,7 +10,8 @@ function get_db_connection() {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+    
+    $conn->set_charset("utf8mb4");
     return $conn;
 }
 ?>
